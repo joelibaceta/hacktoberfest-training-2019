1459c369d6ba482f79a4c07ffe289bc425e0f695
joel ibaceta <mail@joelibaceta.com>
2019-10-01 15:16:18 -0500

    config

--- update.sh
+++ update.sh
@@ -2,7 +2,10 @@
 echo "Pushing..."
 git add .
 git commit -m "updating plots"
-git remote remove origin
+git pull origin gh-pages
+git remote rm origin
+git config --global user.name "bot"
+git config --global user.email "bot@github.com"
 git remote add origin https://joelibaceta:${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
 git pull origin gh-pages
 git push -u -f origin gh-pages
