77990232b03ccd7a5ec5c937fedc52fac8210bf8
joel ibaceta <mail@joelibaceta.com>
2019-10-01 15:03:53 -0500

    remote

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "Pushing..." 
-git remote add virtual https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
+echo "Pushing..."
 git add .
 git commit -m "updating plots"
+git remote add virtual https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
 git pull virtual master
-git push -f virtual images
+git push -u -f virtual images
