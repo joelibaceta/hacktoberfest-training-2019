6818487ba5006a6a01b040696670abb9ab3c9f8a
joel ibaceta <mail@joelibaceta.com>
2019-10-01 15:17:44 -0500

    cloning

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 echo "Pushing..."
+git clone https://joelibaceta:${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
+
+cd hacktoberfest-2019
 git add .
-git commit -m "updating plots"
-git pull origin gh-pages
-git remote rm origin
+git commit -m "updating plots" 
+
 git config --global user.name "bot"
 git config --global user.email "bot@github.com"
-git remote add origin https://joelibaceta:${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
+
 git pull origin gh-pages
 git push -u -f origin gh-pages
