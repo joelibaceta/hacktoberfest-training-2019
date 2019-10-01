7e2cfa570e0854c8945e6a8edd90222ca2c8ac70
joel ibaceta <mail@joelibaceta.com>
2019-10-01 15:08:04 -0500

    using user

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ echo "Pushing..."
 git add .
 git commit -m "updating plots"
 git remote remove origin
-git remote add origin https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
-git pull origin master
+git remote add origin https://joelibaceta:${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
+git pull origin images
 git push -u -f origin images
