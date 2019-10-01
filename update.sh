20a2fedd06f55ca7e49a7e9c892ffd606eeb7da1
joel ibaceta <mail@joelibaceta.com>
2019-10-01 15:11:49 -0500

    using gh-pages

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ echo "Pushing..."
 git add .
 git commit -m "updating plots"
 git remote add virtual https://joelibaceta:${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
-git pull virtual master
-git push -u -f virtual master
+git pull virtual gh-pages
+git push -u -f virtual gh-pages
