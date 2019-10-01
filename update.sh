8cfd72b9ebe1bb1ca93b8934ccab8f36851281a5
joel ibaceta <mail@joelibaceta.com>
2019-10-01 13:04:30 -0500

    fixing repo url

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 echo "Building..."
 python plot.py
 echo "Pushing..."
-git remote add virtual https://${GITHUB_TOKEN}@github.com/joelibaceta/Hacktoberfest2019.git
+git remote add virtual https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
 git add .
 git commit -m "updating plots"
 git pull virtual master
