a1cb1b3896cfe19ae5e9fc2c8f0b7a0c03a1d0d7
joel ibaceta <mail@joelibaceta.com>
2019-10-01 13:23:14 -0500

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-echo "Pushing..."
-git remote remove origin
-git remote add origin https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
+echo "Pushing..." 
+git remote add virtual https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
 git add .
 git commit -m "updating plots"
-git pull origin master
-git push origin master
+git pull virtual master
+git push virtual images
