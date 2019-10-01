e1da157ccce06298a0fa25f982d861e1cf8b8618
joel ibaceta <mail@joelibaceta.com>
2019-10-01 15:07:06 -0500

    using images branch

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 echo "Pushing..."
 git add .
 git commit -m "updating plots"
-git remote add other https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
-git pull other master
-git push -u -f other images
+git remote remove origin
+git remote add origin https://${GITHUB_TOKEN}@github.com/joelibaceta/hacktoberfest-2019.git
+git pull origin master
+git push -u -f origin images
