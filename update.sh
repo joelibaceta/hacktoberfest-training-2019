c80347ee25aed245266f5a41dcccf4288ed38530
joel ibaceta <mail@joelibaceta.com>
2019-10-01 13:02:51 -0500

    pull before push

--- update.sh
+++ update.sh
@@ -5,4 +5,5 @@ echo "Pushing..."
 git remote add virtual https://${GITHUB_TOKEN}@github.com/joelibaceta/Hacktoberfest2019.git
 git add .
 git commit -m "updating plots"
+git pull virtual master
 git push virtual master
